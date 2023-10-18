934832de82a593977ac69faee054bec47367afde
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-18 16:51:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,92 @@
-// JavaScript code for Mean calculation and displaying it in a bar chart
+/* styles.css */
 
-let data = [];
-let selectedDataIndex = -1;
-
-// Menampilkan tab Mean saat halaman dimuat
-showMeanTab();
-
-// Menampilkan konten tab Mean
-function showMeanTab() {
-    document.getElementById('mean-tab').style.display = 'block';
-    document.getElementById('modus-tab').style.display = 'none';
-    document.getElementById('median-tab').style.display = 'none';
-
-    // Contoh: Menampilkan data Mean dalam grafik
-    const values = data.map(item => item.value);
-    const mean = calculateMean(values);
-
-    const meanChart = new Chart(document.getElementById('mean-chart'), {
-        type: 'bar',
-        data: {
-            labels: ['Mean'],
-            datasets: [{
-                label: 'Mean',
-                data: [mean],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
 }
 
-// Function untuk menghitung Mean
-function calculateMean(values) {
-    if (values.length === 0) return 0;
-    const sum = values.reduce((total, value) => total + value, 0);
-    return sum / values.length;
+.container {
+    background-color: #fff;
+    margin: 20px;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-// ... (Fungsi lainnya untuk CRUD, perhitungan statistik lainnya, dan tampilan)
+h1 {
+    text-align: center;
+    color: #333;
+}
 
+.data-entry {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.data-entry input {
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.action-button {
+    background-color: #4299e1;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+}
+
+.action-button:hover {
+    background-color: #2b6cb0;
+}
+
+#update-button, #cancel-button {
+    display: none;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+thead {
+    background-color: #4299e1;
+    color: #fff;
+}
+
+table, th, td {
+    border: 1px solid #ccc;
+}
+
+th, td {
+    text-align: center;
+    padding: 10px;
+}
+
+.edit-button {
+    background-color: #63ba69;
+}
+
+.edit-button:hover {
+    background-color: #4e9e57;
+}
+
+.delete-button {
+    background-color: #d9534f;
+}
+
+.delete-button:hover {
+    background-color: #c9302c;
+}
+
+.statistics {
+    margin-top: 20px;
+    text-align: center;
+    font-weight: bold;
+}
