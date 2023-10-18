a9976a064b7d6960f8f6c2579a017a2b027eec05
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-18 16:33:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,54 @@
 const data = [];
 let selectedDataIndex = -1;
 
-// Tampilan pertama saat halaman dimuat
-function showInitialView() {
-    document.getElementById('mean-tab').style.display = 'none';
+// JavaScript code for Mean calculation and displaying it in a bar chart
+
+let data = [];
+let selectedDataIndex = -1;
+
+// Menampilkan tab Mean saat halaman dimuat
+showMeanTab();
+
+// Menampilkan konten tab Mean
+function showMeanTab() {
+    document.getElementById('mean-tab').style.display = 'block';
     document.getElementById('modus-tab').style.display = 'none';
     document.getElementById('median-tab').style.display = 'none';
-    document.getElementById('mean-tab').innerHTML = ''; // Isi konten dapat ditambahkan di sini
-    document.getElementById('modus-tab').innerHTML = ''; // Isi konten dapat ditambahkan di sini
-    document.getElementById('median-tab').innerHTML = ''; // Isi konten dapat ditambahkan di sini
+
+    // Contoh: Menampilkan data Mean dalam grafik
+    const values = data.map(item => item.value);
+    const mean = calculateMean(values);
+
+    const meanChart = new Chart(document.getElementById('mean-chart'), {
+        type: 'bar',
+        data: {
+            labels: ['Mean'],
+            datasets: [{
+                label: 'Mean',
+                data: [mean],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+// Function untuk menghitung Mean
+function calculateMean(values) {
+    if (values.length === 0) return 0;
+    const sum = values.reduce((total, value) => total + value, 0);
+    return sum / values.length;
+}
+
+
 
     // Mengganti tampilan saat tab diklik
     document.getElementById('tab-mean').addEventListener('click', () => {
